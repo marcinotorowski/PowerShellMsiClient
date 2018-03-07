30cbf7c899702e1410b9b7746ecdea062ef20bd3
marcinotorowski <marcin.otorowski+git@gmail.com>
2018-03-07 22:43:55 +0100

    Updated the bootstrapper to reflect the folder change

--- src/MsiClient.psm1
+++ src/MsiClient.psm1
@@ -1,9 +1,9 @@
-$files_objects = @( Get-ChildItem -Path $PSScriptRoot\Objects\*.ps1 -ErrorAction SilentlyContinue )
+$files_domain = @( Get-ChildItem -Path $PSScriptRoot\Domain\*.ps1 -ErrorAction SilentlyContinue )
 $files_private = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -ErrorAction SilentlyContinue )
 $files_public  = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -ErrorAction SilentlyContinue )
 
 # Dot the sources
-foreach($file in @($files_objects + $files_private + $files_public))
+foreach($file in @($files_domain + $files_private + $files_public))
 {
 	Try
 	{
